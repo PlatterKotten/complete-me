f47d99f8e7b1654f6e7f24943d9152d755f84d7a
Petar Björkman <bagar@Petars-MacBook-Pro.local>
2019-11-06 15:28:33 +0100

    Done

--- Program.cs
+++ Program.cs
@@ -11,34 +11,84 @@ namespace complete_me
         static void Main(string[] args)
         {
             Random slumpat = new Random(); 
-            int speltal = slumpat.Next(); 
+            int speltal = slumpat.Next(1,21); 
          
             bool spela = true; 
 
-            while (!spela)
+            while (spela)
             {
+               
+                    
                 Console.Write("\n\tGissa på ett tal mellan 1 och 20: ");
-                int tal = Convert.ToInt32(Console.ReadLine());
+                string stringTal = Console.ReadLine();
+                //int tal = Convert.ToInt32(Console.ReadLine());
+              
+               
 
+                  switch(Int32.TryParse(stringTal,out int tal))
+                  {
+                      case true:
+                            SwitchCase(tal);
+                          break;
+
+
+
+
+                      
+                 
+                         default:
+                         if(stringTal=="q".ToUpper()||stringTal=="Q".ToLower())
+                         {
+                               spela = false;
+                               Console.WriteLine("\tDu valde att avsluta spelet!");
+                         }
+                         else
+                         {
+                              Console.WriteLine("\tSkriv in en siffra pöjk!");
+                         }
+               
+                        
+                         break;
+                           
+                         
+                  }
+                  
+               
+
+               
+                
+                
+
+            }
+            void SwitchCase(int tal)
+            {
+               
+                    
+               
                 if (tal < speltal)
                 {
                     Console.WriteLine("\tDet inmatade talet " + tal + " är för litet, försök igen.");
+              
                 }
 
                 if (tal > speltal)
                 {
-                    Console.WriteLine("\tDet inmatade talet " + tal  " är för stort, försök igen.");
+                    Console.WriteLine("\tDet inmatade talet " + tal + " är för stort, försök igen.");
+                   
                 }
 
-                if (tal = speltal)
+                if (tal == speltal)
                 {
                     Console.WriteLine("\tGrattis, du gissade rätt!");
+                   
+                    spela = false;
                 }
-
-                spela = false;
-
+             
+             
             }
+            
 
         }
-    }
+      }
+    
 }
